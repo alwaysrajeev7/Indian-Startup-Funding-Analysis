fa5c76584b88f3c96ccf4bc07e1a3ace34821b36
Rajeev <rajeevnayantripathi36@gmail.com>
2024-02-08 17:59:56 +0530

    Investor Analysis and Overall Analysis done

--- app.py
+++ app.py
@@ -2,9 +2,9 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 
-st.set_page_config(layout='wide',page_title= 'Startup-Analysis')
+st.set_page_config(layout='wide',page_title= 'Indian-Startup-Analysis')
 
-df = pd.read_csv('startup_cleaned.csv')
+df = pd.read_csv('startup_cleaned-by-rajeev.csv')
 
 # convert it into pandas datetime object
 df['date'] = pd.to_datetime(df['date'],errors='coerce')
@@ -13,24 +13,32 @@ df['month'] = df['date'].dt.month
 df['date'] = df['date'].dt.date
 
 # Cleaning Cities Name
-df.replace('Ahmedabad','Ahemdabad',inplace = True)
-df.replace(['Bengaluru','Bangalore / SFO','Bangalore / San Mateo','Bangalore/ Bangkok','Banglore'],'Bangalore',inplace = True)
-df.replace('Bhubneswar','bhubaneswar',inplace = True)
-df.replace(['Delhi','New Delhi / US','New Delhi/ Houston','Nw Delhi'],'New Delhi',inplace = True)
-df.replace(['Mumbai / Global','Mumbai/Bengaluru'],'Mumbai',inplace = True)
-df.replace(['Pune / Dubai','Pune / Singapore','Pune / US','Pune/Seattle'],'Pune',inplace = True)
-df.replace('Gurgaon','Gurugram',inplace=True)
+# df.replace('Ahmedabad','Ahemdabad',inplace = True)
+# df.replace(['Bengaluru','Bangalore / SFO','Bangalore / San Mateo','Bangalore/ Bangkok','Banglore'],'Bangalore',inplace = True)
+# df.replace('Bhubneswar','bhubaneswar',inplace = True)
+# df.replace(['Delhi','New Delhi / US','New Delhi/ Houston','Nw Delhi'],'New Delhi',inplace = True)
+# df.replace(['Mumbai / Global','Mumbai/Bengaluru'],'Mumbai',inplace = True)
+# df.replace(['Pune / Dubai','Pune / Singapore','Pune / US','Pune/Seattle'],'Pune',inplace = True)
+# df.replace('Gurgaon','Gurugram',inplace=True)
 
 # Cleaning Rounds Names
-df.replace(['Pre Series A', 'Pre-series A', 'pre-Series A', 'pre-series A'], 'Pre-Series A', inplace=True)
-df.replace(['Angel / Seed Funding', 'Seed/ Angel Funding', 'Seed / Angle Funding', 'Seed/ Angel Funding', 'Seed/Angel Funding','Seed / Angel Funding'], 'Seed/Angel Funding', inplace=True)
-df.replace('Seed funding', 'Seed Funding', inplace=True)
+# df.replace(['Pre Series A', 'Pre-series A', 'pre-Series A', 'pre-series A'], 'Pre-Series A', inplace=True)
+# df.replace(['Angel / Seed Funding', 'Seed/ Angel Funding', 'Seed / Angle Funding', 'Seed/ Angel Funding', 'Seed/Angel Funding','Seed / Angel Funding'], 'Seed/Angel Funding', inplace=True)
+# df.replace(['Seed Round','Seed','Seed funding'], 'Seed Funding', inplace=True)
+# df.replace('Debt-Funding','Debt Funding',inplace = True)
+# df.replace('Private','Private Funding',inplace = True)
+# df.replace('Angel','Angel Round',inplace = True)
+# df.replace('Private Equity Round','Private Equity',inplace = True)
+
 
 # cleaning vertical name
-df.replace(['ECommerce', 'eCommerce', 'Ecommerce', 'E-commerce'],'E-Commerce', inplace=True)
-df.replace(['E-Tech', 'EdTech'],'Ed-Tech', inplace=True)
-df.replace('FinTech','Fin-Tech', inplace=True)
+# df.replace(['ECommerce', 'eCommerce', 'Ecommerce', 'E-commerce'],'E-Commerce', inplace=True)
+# df.replace(['E-Tech', 'EdTech'],'Ed-Tech', inplace=True)
+# df.replace('FinTech','Fin-Tech', inplace=True)
 
+# cleaning company name
+# df.replace('DST Global and Lightspeed Venture Partners\\\\xe2\\\\x80\\\\x99 global fund.','DST Global',inplace = True)
+# df.replace(['SoftBank Group','Softbank Group','Softbank Group Corp'],'Softbank',inplace = True)
 
 def load_overall_analysis_details():
     st.title('Overall Analysis')
@@ -40,17 +48,17 @@ def load_overall_analysis_details():
     with col1:
         # Total amount invested
         total = round(df['amount'].sum())
-        st.metric(label='Total Amount Invested',value = str(total)+'cr')
+        st.metric(label='Total Amount Invested',value = '₹'+str(total)+'cr')
 
     with col2:
         # maximum amount infused in a startup
         max_funding = df.groupby('startup')['amount'].max().sort_values(ascending=False).head(1).values[0]
-        st.metric(label='Maximum Funding size ',value = str(max_funding)+'cr')
+        st.metric(label='Maximum Funding size ',value = '₹'+str(round(max_funding))+'cr')
 
     with col3:
         # average funding amount
         avg_funding = round((df.groupby('startup')['amount'].sum()).mean())
-        st.metric(label='Average Funding size ', value = str(avg_funding)+'cr')
+        st.metric(label='Average Funding size ', value = '₹'+str(avg_funding)+'cr')
 
     with col4:
         # total funded startup
@@ -63,20 +71,24 @@ def load_overall_analysis_details():
     with col5:
     #MoM chart -> Total + Count
 
-        st.header('Month on Month Graph')
-        selected_option = st.selectbox('Select Type',['Total','Count'])
+        st.header('Month on Month Investment Graph')
+        selected_option = st.selectbox('Select Type',['Total Amount',' StartUp Count'])
 
-        if selected_option == 'Total':
+        if selected_option == 'Total Amount':
             temp_df = df.groupby(['year', 'month'])['amount'].sum().reset_index()
             temp_df['x_axis'] = temp_df['month'].astype('str') + '-' + temp_df['year'].astype('str')
 
+            fig5, ax5 = plt.subplots()
+            ax5.plot(temp_df['x_axis'], temp_df['amount'])
+            st.pyplot(fig5)
+
         else:
-            temp_df = df.groupby(['year', 'month'])['amount'].count().reset_index()
+            temp_df = df.groupby(['year', 'month'])['startup'].count().reset_index()
             temp_df['x_axis'] = temp_df['month'].astype('str') + '-' + temp_df['year'].astype('str')
 
-        fig5, ax5 = plt.subplots()
-        ax5.plot(temp_df['x_axis'], temp_df['amount'])
-        st.pyplot(fig5)
+            fig5, ax5 = plt.subplots()
+            ax5.plot(temp_df['x_axis'], temp_df['startup'])
+            st.pyplot(fig5)
 
 
     with col6:
@@ -128,7 +140,6 @@ def load_overall_analysis_details():
 
             top_df['year'] = top_df['year'].astype('str')
             top_df['year'] = top_df['year'].str.replace(',','')
-            #top_df['year'] = top_df['year'].astype('int')
             st.dataframe(top_df)
 
         else:
@@ -137,18 +148,20 @@ def load_overall_analysis_details():
     with col10:
         # Top investors
         st.subheader('Top Investors')
-        st.dataframe(df.groupby(['investors'])['amount'].sum().sort_values(ascending=False).reset_index().head(15))
+        top_investors = df.groupby(['investors'])['amount'].sum().sort_values(ascending=False).reset_index().head(20)
+        l = sorted(set(top_investors['investors'].str.split(',').sum()))
+        st.dataframe(df[df['investors'].isin(l)].groupby('investors')['amount'].sum().sort_values(ascending=False).reset_index())
 
 
 def load_investor_details(investor):
 
     st.title(investor)
 
-    # 1. load recent 5 investments of the investor
+    # 1. load recent 10 investments of the investor
 
-    last5_df = df[df['investors'].str.contains(investor)].sort_values(by='date', ascending=False,ignore_index=True).head(10)[['date', 'startup', 'vertical', 'city', 'round', 'amount']]
     st.subheader('Most Recent Investments')
-    st.dataframe(last5_df)
+    last10_df = df[df['investors'].str.contains(investor)].sort_values(by='date', ascending=False,ignore_index=True).head(10)[['date','startup','vertical','city','round','amount']]
+    st.dataframe(last10_df)
 
     # 2. biggest investments
 
@@ -177,7 +190,7 @@ def load_investor_details(investor):
 
     # stage -> pie
     with col3:
-        stage_series = df[df['investors'].str.contains(investor)].groupby('round')['amount'].sum().sort_values(ascending=False).head(4)
+        stage_series = df[df['investors'].str.contains(investor)].groupby('round')['amount'].sum().sort_values(ascending=False).head()
         st.subheader('Stages Invested')
         fig2,ax2 = plt.subplots()
         ax2.pie(stage_series,labels = stage_series.index)
@@ -186,7 +199,7 @@ def load_investor_details(investor):
     # city -> pie
     with col4:
         city_series = df[df['investors'].str.contains(investor)].groupby('city')['amount'].sum().sort_values(ascending=False).head(7)
-        st.subheader('Cities with most investments')
+        st.subheader('Cities with most Investments')
         fig3,ax3 = plt.subplots()
         ax3.pie(city_series,labels = city_series.index)
         st.pyplot(fig3)
@@ -206,21 +219,21 @@ def load_investor_details(investor):
     # Similar Investors
 
         st.subheader('Similar Investors')
-        l = df[df['investors'] == investor]['vertical'].value_counts().head(3).index.tolist()
-        without_undis = df[df['investors'].str.contains('Undisclosed Investors', case=False)]
-        new = df.drop(index = without_undis.index)
+        l = df[df['investors'].str.contains(investor)]['vertical'].value_counts().head(3).index.tolist()
+        undis_investors = df[df['investors'].str.contains('Undisclosed Investors', case=False)]
+        new = df.drop(index = undis_investors.index)
         new = new[new['vertical'].isin(l)]['investors'].value_counts().reset_index().head(10).drop(columns = 'count')
         st.dataframe(new)
 
 
-
-st.sidebar.title('Startup Funding Analysis')
+st.sidebar.title('Indian StartUps Funding Analysis')
 
 option = st.sidebar.selectbox('Select One',['Overall Analysis','StartUp Analysis','Investor Analysis'])
 
 if option == 'Overall Analysis':
-
-    load_overall_analysis_details()
+    # btn1 = st.sidebar.button('Go')
+    # if btn1:
+        load_overall_analysis_details()
 
 elif option == 'StartUp Analysis':
 
@@ -229,8 +242,12 @@ elif option == 'StartUp Analysis':
     st.title('StartUp Analysis')
 
 else:
-    selected_investor = st.sidebar.selectbox('Select Investor',sorted(set(df['investors'].str.split(',').sum())))
-    btn3 = st.sidebar.button('Find Investor Details')
+    df.replace(['SoftBank Group', 'Softbank Group', 'Softbank Group Corp'], 'Softbank', inplace=True)
+    investors_list = sorted(set(df['investors'].str.split(',').sum()))
+    investors_list.pop(0)
+
+    selected_investor = st.sidebar.selectbox('Select Investor',investors_list)
+    btn3 = st.sidebar.button('Go')
 
     if btn3:
         load_investor_details(selected_investor)
